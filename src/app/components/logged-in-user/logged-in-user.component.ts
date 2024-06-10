030d272f21d86e75040956f642f58304c19b699a
Jess van Houts <jvanhouts@protonmail.com>
2024-06-10 12:02:47 +0200

    refactor

--- src/app/components/logged-in-user/logged-in-user.component.ts
+++ src/app/components/logged-in-user/logged-in-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../../services/auth.service';
 import { Subscription } from 'rxjs';
 import { Router, RouterModule } from '@angular/router';
 import { NgIf } from '@angular/common';
